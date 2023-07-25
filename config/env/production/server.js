7d4bb3748f8ba4e4b6687286dcb622205ab1cae8
alam.saud <alam.saud@mi-c3.com>
2023-07-25 18:16:37 +0600

    configue

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,4 +2,5 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'https://strapi-on-azure-ipractest.azurewebsites.net',
+  keys: ['whMD67GmaUjjntWus1/Kag==','pVEUr3VCBSn28TEiVlB+gQ==','+7Pw5AAVOAczUAJ8nGl1jg==','D88uaf4HUYmqYOOrkn6FCw==']
 });
